eb9a4a881f02a1165ec070430f53107f29934aaf
DUDE <nisbis194@gmail.com>
2025-01-04 16:48:54 +0545

    learnt using props and usestate hook in ts

--- Ts/src/App.tsx
+++ Ts/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type AppProps = {
+  hello: string;
+};
 
+export default function App(props: AppProps) {
+  const [edit, setEdit] = useState<number>(0);
+  const [texts, setTexts] = useState<string>("");
+  function Add() {
+    setEdit(edit + 1);
+  }
+
+  function handlesubmit(event: React.ChangeEvent<HTMLInputElement>) {
+    setTexts(event.target.value);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {props.hello}
+      {edit}
+      <button onClick={Add}>click me </button>
+      <br />
+      <input type="text" onChange={handlesubmit} />
+      <br />
+      <p>{texts}</p>
+    </div>
+  );
 }
-
-export default App
